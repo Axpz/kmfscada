c83157bbeaae4cc8465720bfa42de3a911982337
Xianping <axpzhang@gmail.com>
2025-07-10 23:18:36 +0800

    update doc

--- history.sh
+++ history.sh
@@ -1,2 +1,9 @@
 docker exec -it scada-db psql -U postgres -d scada
 
+\pset pager off
+
+
+docker exec -it supabase-db psql -U supabase_admin -d _supabase
+
+SELECT * FROM _supavisor.tenants LIMIT 10;
+SELECT * FROM _supavisor.users LIMIT 10;
